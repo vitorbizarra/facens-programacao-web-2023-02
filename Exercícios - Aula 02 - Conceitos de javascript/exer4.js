3f3b57e58a544e7cd6d09731aa080aa3ebb60bcf
vitorbizarra <vitorbizarracontato@gmail.com>
2023-08-19 00:53:31 +0000

    :sparkles: feat: Exer. 4 - Aula 02

--- Exercícios - Aula 02 - Conceitos de javascript/exer4.js
+++ Exercícios - Aula 02 - Conceitos de javascript/exer4.js
@@ -2,3 +2,11 @@
  * Crie uma função que verifica se um número inteiro passado como parâmetro é
  * divisível por 3 e retorne true ou false.
  */
+
+const prompt = require('prompt-sync')();
+
+const number = parseInt(prompt('Digite um número para saber se ele é divisível por 3: '))
+
+const divisibleByThree = (number) => (number % 3 == 0);
+
+console.log((divisibleByThree(number)) ? 'Sim' : 'Não');
