3debf2ffe2d9154878648263bf52bb0b5bec0af5
vitorbizarra <vitorbizarracontato@gmail.com>
2023-08-30 23:34:31 +0000

    :construction: Exercício 3 - Aula 02

--- Exercícios - Aula 02 - Conceitos de javascript/exer3.js
+++ Exercícios - Aula 02 - Conceitos de javascript/exer3.js
@@ -4,3 +4,17 @@
  * financeira sob o regime de juros simples e a segunda retornará o valor da
  * aplicação sob o regime de juros compostos.
  */
+
+const jurosSimples = (capitalInicial, taxaDeJuros, tempoDeAplicacao) => {
+    return capitalInicial + (taxaDeJuros * tempoDeAplicacao)
+}
+
+const jurosCompostos = () => {
+    let resultado = 0;
+
+    for(i = 0; i < tempoDeAplicacao; i++){
+        resultado = resultado + taxaDeJuros;   
+    }
+
+    
+}
