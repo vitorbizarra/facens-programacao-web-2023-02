f20411d19e310b41a2ee79c9c630430ab2ae8f2c
vitorbizarra <vitorbizarracontato@gmail.com>
2023-08-30 23:34:19 +0000

    :sparkles: feat: Exer 2 e 5 - Aula 02

--- Exercícios - Aula 02 - Conceitos de javascript/exer2.js
+++ Exercícios - Aula 02 - Conceitos de javascript/exer2.js
@@ -7,3 +7,7 @@
  * Desenvolva uma função JavaScript para que ela receba um valor como
  * 0.30000000000000004 e retorne R$0,30 (observe a vírgula e o ponto).
  */
+
+const moneyFormat = (number) => new Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(number);
+
+console.log(moneyFormat(0.30000000000000004))
--- Exercícios - Aula 02 - Conceitos de javascript/exer5.js
+++ Exercícios - Aula 02 - Conceitos de javascript/exer5.js
@@ -2,3 +2,37 @@
  * Escreva uma função que recebe um vetor de números inteiros e mostre como
  * resultado qual é o menor e qual é o maior número.
  */
+
+const prompt = require('prompt-sync')();
+
+const getMenorMaior = (numbers) => {
+    let max = Number.MIN_VALUE
+    let min = Number.MAX_VALUE
+
+    numbers.forEach(number => {
+        if(number > max){
+            max = number;
+        }
+
+        if (number < min){
+            min = number;
+        }
+    });
+
+    console.log(`O menor número é ${min} e o maior é ${max}`)
+}
+
+let loop = true;
+let numbers = [];
+
+while(loop){
+    var number = parseFloat(prompt('Digite um número: '));
+    numbers.push(number);
+
+    var answer = prompt('Deseja adicionar mais números? (s/n) ');
+    loop = (answer == 's');
+}
+
+console.log(numbers);
+
+getMenorMaior(numbers);
